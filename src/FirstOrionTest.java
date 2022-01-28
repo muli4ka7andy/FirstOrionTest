cb56722cf0620ce2db489f41cd8d898dc5aac3cd
amulenko <anmulenko2@gmail.com>
2022-01-28 17:00:24 +0200

    Test 1-28-2022

--- src/FirstOrionTest.java
+++ src/FirstOrionTest.java
@@ -4,44 +4,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.nio.file.Watchable;
+
 import java.util.ArrayList;
 
 public class FirstOrionTest {
     public static WebDriver driver;
+    public static final double actual = 67200000.00;
 
     @Test
-    public void Test() throws AWTException, InterruptedException {
+    public void Test(){
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        String oldTab = driver.getWindowHandle();
         driver.findElement(By.xpath("//*[contains(text(), 'Sutinku')]")).click();
         driver.findElement(By.name("q")).sendKeys("sql table online" + Keys.chord(Keys.ENTER));
         String result = driver.findElement(By.id("result-stats")).getText();
-        Assert.assertNotEquals("There are Apie 67 200 000 rezult. (0,34 sek.)", "There are " + result);
-        driver.findElement(By.xpath("//a[@aria-label='Page 5']")).click();
+        String[] words = result.split("\\s");
+        String expected = words[1]+words[2]+words[3];
+        double result2 = Double.parseDouble(expected);
+        double delta = actual - result2;
+        double result3 = delta + result2;
+        Assert.assertEquals(actual, result3 ,0.2);
+        driver.findElement(By.xpath("//a[@aria-label='Page 6']")).click();
         Actions actions = new Actions(driver);
-
-
         WebElement link = driver.findElement(By.xpath("//*[contains(text(), 'SQL Exercises, Practice, Solution')]"));
         actions.contextClick(link).build().perform();
-        Robot robot = new Robot();
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
         robot.keyPress(KeyEvent.VK_DOWN);
         robot.keyRelease(KeyEvent.VK_DOWN);
         robot.keyPress(KeyEvent.VK_ENTER);
         robot.keyRelease(KeyEvent.VK_ENTER);
-        Thread.sleep(1000);
-        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
 
         String backgroundColor = driver.findElement(By.cssSelector("body")).getCssValue("background-color");
